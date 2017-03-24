3e51a80afeb0ea71fa620e1008906d1c45d56d85
rogerbf <roger@bergfalk.nu>
2017-03-24 23:03:06 +0100

    experimentation

--- source/factory.js
+++ source/factory.js
@@ -1,1 +1,10 @@
-module.exports = ({ spawn, args }, name, configuration = {}) => {}
+module.exports = ({ spawn, args }, name, configuration = {}) => {
+  return options => {
+    return spawn(
+      name,
+      Object.keys(configuration.args)
+      .reduce((acc, action) => args[action](configuration.args[action]), args)(options),
+      configuration.options
+    )
+  }
+}
